1e37a2caeaabbc70ca5f8e6e26220fb0fd058382
Stephen Hack <Stephen Hack>
2016-08-22 14:23:56 -0400

    stephenhack

---
+++ StephenHack/algorithms/mostsignificant.js
@@ -0,0 +1,17 @@
+function mostsignificant (significant) {
+	var arr = significant
+
+	while (i !== 0) {
+		i++
+	}
+
+
+}
+
+function statsUntilDouble (min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+	math.
+
+
+
+}

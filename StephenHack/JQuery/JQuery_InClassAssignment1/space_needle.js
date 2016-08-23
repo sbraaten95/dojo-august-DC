b552aaab3bc01adae825a267ca1b8381a0be7d59
Stephen Hack <Stephen Hack>
2016-08-23 16:41:35 -0400

    August232016

---
+++ StephenHack/JQuery/JQuery_InClassAssignment1/space_needle.js
@@ -0,0 +1,24 @@
+for (var i = 0; i <= 3; i++) {
+
+	console.log("                              ||");
+};
+
+for (var i = 0; i < 4; i++) {
+	row = "                             ";
+
+	for (var i = 0; i < 4; i++) {
+		var sum = "";
+		if (i > 0)
+		sum += row + "_ _ /"
+		console.log(sum)
+	};
+	
+
+
+	// for (var j = 0; j < ; i++) {
+	// 	Things[i]
+	// };
+};
+
+
+
